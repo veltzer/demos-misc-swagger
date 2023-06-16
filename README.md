865384a894f355b165e692a6ed3974c4b885ad24
Mark Veltzer <mark.veltzer@gmail.com>
2023-06-16 21:38:08 +0300

    no commit message given

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # demos-swagger
 Demos for the swagger tools
+
+Real documentation for swagger-ui is here:
+https://www.npmjs.com/package/swagger-ui
+AND NOT on docker.hub where all the links are dead!
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 # docker pull swaggerapi/swagger-ui
-docker run -p 8080:8080 -e SWAGGER_YAML=api.yaml -v api.yaml:api.yaml swaggerapi/swagger-ui
+docker run -p 8080:8080 -e SWAGGER_YAML=/api/api//api.yaml -v "${PWD}:/api" swaggerapi/swagger-ui
