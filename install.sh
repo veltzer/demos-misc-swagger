182414b2e5be2c8d33b1f0528db2c0dab6443214
Mark Veltzer <mark.veltzer@gmail.com>
2023-06-16 23:45:25 +0300

    no commit message given

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 # This does not work because of libx11 dependency not being installable
 # brew install swagger-codegen
 VERSION="2.4.32"
-mkdir downloads
+mkdir -p downloads
 wget "https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/${VERSION}/swagger-codegen-cli-${VERSION}.jar" -O "downloads/swagger-codegen-cli.jar"
