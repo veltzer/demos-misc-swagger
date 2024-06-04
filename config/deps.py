7f0812442526aa6139f3e5e53052459f457baf0d
Mark Veltzer <mark.veltzer@gmail.com>
2024-06-04 12:47:33 +0300

    no commit message given

--- config/deps.py
+++ config/deps.py
@@ -1,6 +1,13 @@
 """
 os level dependencies for this project
 """
+
+import platform
+
 packages = [
-    "reserialize",
 ]
+
+desktop = platform.freedesktop_os_release()
+VERSION_ID = desktop["VERSION_ID"]
+if VERSION_ID == "24.04":
+    packages.append("reserialize")
